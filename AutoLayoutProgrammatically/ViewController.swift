b9e885d457987c091cf3a3dee1b227c56f9bbe20
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2022-09-23 14:05:47 +0200

    Initial Commit

---
+++ AutoLayoutProgrammatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutProgrammatically
+//
+//  Created by Pat on 2022/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
