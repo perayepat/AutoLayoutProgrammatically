870758d5e071e350cc0831583929cfe9e69b325d
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2022-09-24 09:08:34 +0200

    Buttons were added to the collection view controller

--- AutoLayoutProgrammatically/PageCell.swift
+++ AutoLayoutProgrammatically/PageCell.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+//MARK: - View
 class PageCell: UICollectionViewCell{
     
     var page: Page?{
--- AutoLayoutProgrammatically/SwippingController.swift
+++ AutoLayoutProgrammatically/SwippingController.swift
@@ -5,6 +5,8 @@
 //  Created by Pat on 2022/09/23.
 //
 
+//MARK: - Controller
+///make sure the view knows the right objects to display
 
 import UIKit
 
@@ -19,16 +21,71 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
                  Page(imageName: "diet", headerText: "Recover your diet plans",
                       bodyText: "Make a dietery plan that works for you  and that takes you in mind first")]
     
+    //buttons at bottom
+    //encapsulation principles so other classes dont have access to this
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.black, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        let pinkColor = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
+        button.setTitleColor(pinkColor, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private let pageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+        let pinkColor = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 0.8)
+        pc.pageIndicatorTintColor = pinkColor.withAlphaComponent(0.3)
+        pc.currentPageIndicatorTintColor = pinkColor
+        return pc
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupBottomControls()
         collectionView?.register(PageCell.self, forCellWithReuseIdentifier: "cellId")
         collectionView?.isPagingEnabled = true
+        collectionView?.showsVerticalScrollIndicator = false
+        collectionView?.showsHorizontalScrollIndicator = false
+        
     }
 
     
 }
-
+extension SwipingController{
+    fileprivate func setupBottomControls(){
+//        view.addSubview(previousButton)
+        
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        let greenView = UIView()
+        greenView.backgroundColor = .green
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton,pageControl,nextButton])
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlsStackView.distribution = .fillProportionally
+        view.addSubview(bottomControlsStackView)
+        
+        NSLayoutConstraint.activate([
+            bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: 20),
+            bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
 extension SwipingController{
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
